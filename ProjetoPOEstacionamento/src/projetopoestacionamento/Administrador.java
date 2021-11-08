b7d6cdce83fa85c271d2b75f2a7790c7e1d5b145
alexvsc1922 <93945053+alexvsc1922@users.noreply.github.com>
2021-11-08 19:58:27 +0000

    ADMIN

---
+++ ProjetoPOEstacionamento/src/projetopoestacionamento/Administrador.java
@@ -0,0 +1,12 @@
+
+package projetopoestacionamento;
+
+import java.io.Serializable;
+
+public class Administrador extends Utilizador implements Serializable {
+  
+    public Administrador(String username, String password, String nome) {
+        super(username, password, nome);
+    }
+   
+}
