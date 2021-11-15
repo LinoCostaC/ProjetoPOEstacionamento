f00092d5510cbd13ec19bd5fb7a2201ead78c4a7
BrunoLeite24 <bruno_leite24@hotmail.com>
2021-11-15 18:20:33 +0000

    parque viatura

--- ProjetoPOEstacionamento/src/backend/Parque.java
+++ ProjetoPOEstacionamento/src/backend/Parque.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package backend;
 
-/**
- *
- * @author bruno
- */
 public class Parque {
+    
+// variaveis de instacia
+    
+    private String nome;
+    private int numeroLugares;
+    private String tipoParque;
+    
+//construtores
+    
+    public Parque() {
+    }
+
+    public Parque(String nome, int numeroLugares, String tipoParque) {
+        this.nome = nome;
+        this.numeroLugares = numeroLugares;
+        this.tipoParque = tipoParque;
+    }
+    
+//get e set de nome
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+//get e set numeroLugares
+    
+    public int getNumeroLugares() {
+        return numeroLugares;
+    }
+
+    public void setNumeroLugares(int numeroLugares) {
+        this.numeroLugares = numeroLugares;
+    }
+
+//get e set tipoParque
+    
+    public String getTipoParque() {
+        return tipoParque;
+    }
+
+    public void setTipoParque(String tipoParque) {
+        this.tipoParque = tipoParque;
+    }
     
 }
--- ProjetoPOEstacionamento/src/backend/Viatura.java
+++ ProjetoPOEstacionamento/src/backend/Viatura.java
@@ -1,5 +1,65 @@
+
 package backend;
 
 public class Viatura {
+    
+//variaveis de instancia
+    
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private String combustivel;
+    
+//construtores
+    
+    public Viatura() {
+    }
+
+    public Viatura(String matricula, String marca, String modelo, String combustivel) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.combustivel = combustivel;
+    }
+    
+//get e set de matricula
+    
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    
+//get e set de marca
+    
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    
+//get e set de modelo
+    
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+        
+//get e set de combustivel
+    
+    public String getCombustivel() {
+        return combustivel;
+    }
+
+    public void setCombustivel(String combustivel) {
+        this.combustivel = combustivel;
+    }
     
 }
