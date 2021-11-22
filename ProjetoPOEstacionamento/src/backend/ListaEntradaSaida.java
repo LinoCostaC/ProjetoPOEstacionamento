ab672f11c02b4e88b45ff2ae768b8d3f0b8a8679
LinoCostaC <56834657+LinoCostaC@users.noreply.github.com>
2021-11-22 19:37:59 +0000

    ListaEntradaSaida

--- ProjetoPOEstacionamento/src/backend/ListaEntradaSaida.java
+++ ProjetoPOEstacionamento/src/backend/ListaEntradaSaida.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package backend;
 
-/**
- *
- * @author LC
- */
-public class ListaEntradaSaida {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+public class ListaEntradaSaida implements Serializable  {
+    //Lista de Entradas e Saidas
+    private ArrayList<EntradaSaida> listaEntradaSaida;
+    
+    //Construtor
+    public ListaEntradaSaida() {
+        listaEntradaSaida = new ArrayList<EntradaSaida>();
+    }
+
+    public ArrayList<EntradaSaida> getListaEntradaSaida() {
+        return listaEntradaSaida;
+    }
+
+    public void setListaEntradaSaida(ArrayList<EntradaSaida> listaEntradaSaida) {
+        this.listaEntradaSaida = listaEntradaSaida;
+    }
+    
+    
+    //Obter todos as entradas e saidas
+    public int getTotalEntradaSaida() {
+        return listaEntradaSaida.size();
+    }
+    
+    public EntradaSaida get(int index) {
+        return listaEntradaSaida.get(index);
+    }
+
+    //Adicionar uma entrada e saida à lista
+    public void adicionarEntradaSaida(EntradaSaida entradasaida) {
+        listaEntradaSaida.add(entradasaida);
+    }
+
+    //Remover uma entrda e saida da lista
+    public void removerEntradaSaida(EntradaSaida entradasaida) {
+        listaEntradaSaida.remove(entradasaida);
+    }
     
 }
+
--- ProjetoPOEstacionamento/src/backend/ListaViatura.java
+++ ProjetoPOEstacionamento/src/backend/ListaViatura.java
@@ -60,6 +60,5 @@ public class ListaViatura implements Serializable {
     
     
     
-    //verifica
 }
 
