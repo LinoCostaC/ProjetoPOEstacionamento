b9686824f4e25f85d6759254553ec55660230abc
LinoCostaC <56834657+LinoCostaC@users.noreply.github.com>
2021-11-22 18:25:03 +0000

    Update Aplicacao.java

--- ProjetoPOEstacionamento/src/backend/Aplicacao.java
+++ ProjetoPOEstacionamento/src/backend/Aplicacao.java
@@ -1,7 +1,27 @@
 
 package backend;
 
+import java.io.Serializable;
 
-public class Aplicacao implements Seri {
+
+public class Aplicacao implements Serializable {
+    private Utilizador userligado;
+    private ListaParque listaParque;
+    private ListaSeguranca listaSeguranca;
+    private ListaUtente listaUtente;
+    private ListaUtilizador listaUtilizador;
+    private ListaViatura listaViatura;
+   
+    
+    public Aplicacao () {
+        listaParque = new ListaParque();
+        listaSeguranca = new ListaSeguranca();
+        listaUtente = new ListaUtente();
+        listaUtilizador = new ListaUtilizador();
+        listaViatura = new ListaViatura();
+        
+    }
+    
+    
     
 }
