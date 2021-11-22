0ab3894d033c1eee1793df3b5cb18529eaa4a1d0
BrunoLeite24 <bruno_leite24@hotmail.com>
2021-11-22 18:27:50 +0000

    Update ParqueAcessoCondicionado.java

--- ProjetoPOEstacionamento/src/backend/ParqueAcessoCondicionado.java
+++ ProjetoPOEstacionamento/src/backend/ParqueAcessoCondicionado.java
@@ -2,6 +2,35 @@
 package backend;
 
 
-public class ParqueAcessoCondicionado {
+public class ParqueAcessoCondicionado extends Parque {
+    // variaveis de instacia
+       private float preco;
+    private String tipoParque;
+    
+     public ParqueAcessoCondicionado(String nome, int numeroLugares, String tipoParque, float preco) {
+        super(nome, numeroLugares);
+        this.tipoParque = tipoParque;
+        this.preco = preco;
+        
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getTipoParque() {
+        return tipoParque;
+    }
+
+    public void setTipoParque(String tipoParque) {
+        this.tipoParque = tipoParque;
+    }
+     
     
 }
+            
+
